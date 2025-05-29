6282a0467f5ccb18ea8018d8fbc8721b6553413d
Seu Nome <alinessgarcia1@gmail.com>
2025-05-29 19:09:12 -0300

    atualização das cores do texto e da linha decorativa na seção Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,9 +17,9 @@ const Hero = () => {
       <span className="text-3xl md:text-5xl lg:text-6xl font-bold text-white block drop-shadow-lg">
   Ministério Bíblico da
 </span>
-      <span className="text-6xl md:text-8xl lg:text-9xl font-bold text-sky-500 block mt-0.5 relative inline-block drop-shadow-xl tracking-tight">
+      <span className="text-6xl md:text-8xl lg:text-9xl font-bold text-blue block mt-0.5 relative inline-block drop-shadow-xl tracking-tight">
         Reconciliação
-        <span className="block h-1 bg-black w-full mt-2 mx-auto rounded-full shadow-lg"></span>
+        <span className="block h-1 bg-blue w-full mt-2 mx-auto rounded-full shadow-lg"></span>
       </span>
     </h1>
 
